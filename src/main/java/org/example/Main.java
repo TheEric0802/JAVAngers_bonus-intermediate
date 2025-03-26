bbd86e4aecce9e7212b6f2fd5bf1eac16854f578
Eric Schmidt <eric0802@web.de>
2025-03-26 15:00:04 +0100

    save input in array and use it as output

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         String[] numbers = {"Eins", "Zwei", "Drei", "Vier", "Fünf", "Sechs", "Sieben", "Acht", "Neun", "Zehn"};
@@ -16,6 +18,28 @@ public class Main {
             System.out.println(i);
         }
 
+        String[] words = new String[0];
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("type a word to save in array, exit program with 'stop': ");
+            String word = input.next();
+
+            if (word.equals("stop")) {
+                break;
+            }
+
+            String[] newWords = new String[words.length + 1];
+            System.arraycopy(words, 0, newWords, 0, words.length);
+            newWords[newWords.length - 1] = word;
+            words = newWords;
+
+            System.out.println("your saved words are: ");
+
+            for (String w : words) {
+                System.out.println(w);
+            }
+        }
     }
 
     public static String getDigitName(int digit, String[] numbers) {
