d92c6c1505c43ba1e9583352f9396c8599e06e91
Eric Schmidt <eric0802@web.de>
2025-03-26 14:54:33 +0100

    add method getDigitName

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,13 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        String[] numbers = {"Eins", "Zwei", "Drei", "Vier", "Fünf", "Sechs", "Sieben", "Acht", "Neun", "Zehn"};
+
+        System.out.println(getDigitName(5, numbers));
+
+    }
+
+    public static String getDigitName(int digit, String[] numbers) {
+        return numbers[digit -1];
     }
 }
